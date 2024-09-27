947c75b07533481edea2e8ce0986c05928ca51b1
Lucas Otávio Silveira Mendes <lucasilveiramendes@gmail.com>
2024-09-27 17:14:44 -0300

    :hammer: Initial CRUD product

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -7,8 +7,95 @@ const router = Router();
 import { Schema, z } from "zod"
 
 const productSchema = z.object({
-    code: z.number(),
+    code: z.number().optional(),
     name: z.string(),
     description: z.string(),
-    price: z.number(),
+    price: z.number()
 })
+
+router.get("/", async (req, res) => {
+    try{
+        const products = await prisma.product.findMany({
+            orderBy: { name: 'asc'},
+            select: {
+                code: true,
+                name: true,
+                description: true,
+                price: true
+            }
+        })
+
+        res.status(200).json(products)
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+router.get("/:code", async (req, res) => {
+    const { code } = req.params;
+    try{
+        const product = await prisma.product.findUnique({
+            where: { code: Number(code) },
+            select: {
+                id: true,
+                code: true,
+                name: true,
+                description: true,
+                price: true
+            }
+        })
+        res.status(200).json(product)
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+router.post("/", async (req, res) => {
+    try {
+        const valid = productSchema.safeParse(req.body)
+
+        if(!valid.success) {
+            res.status(400).json({error: valid.error})
+
+            return;
+        }
+        
+        const codeMax = await prisma.product.findMany({ orderBy: { code: 'desc'}})
+
+        const product = await prisma.product.create({
+            data: {
+                ...valid.data,
+                code: valid.data.code ? valid.data.code : Number(codeMax[0]?.code) + 1
+            }
+        })
+
+        res.status(201).json(product)
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+router.patch("/:id", async (req, res) => {
+    const { id } = req.params
+
+    try {
+        const valid = productSchema.partial()
+        const validPartial = valid.safeParse(req.body)
+
+        if(!validPartial.success) {
+            res.status(400).json({error: validPartial.error})
+
+            return ;
+        }
+
+        const product = await prisma.product.update({
+            where: { id: id},
+            data: validPartial.data
+        })
+
+        res.status(201).json(product)
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+export default router
--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import express from 'express'
 const app = express()
 const port = 3003
 
-app.get('/', (req, res) => {
-    res.send('Criando uma API do zero!')
-    })
+import routeProduct from './controllers/products'
+
+app.use(express.json())
+app.use('/products', routeProduct)
 
 app.listen(port, () => {
-    console.log(`Servidor rodando na porta:${port}`) })
+    console.log(`Servidor rodando na porta:${port}`)
+})
