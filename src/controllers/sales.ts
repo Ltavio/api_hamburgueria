a7416b54808c290f4b82943b05b1416ca30fae95
Lucas Otávio Silveira Mendes <lucasilveiramendes@gmail.com>
2024-10-13 15:37:51 -0300

    :hammer: completed PATCH route Sale

--- src/controllers/sales.ts
+++ src/controllers/sales.ts
@@ -15,7 +15,11 @@ const saleSchema = z.object({
 
 router.get("/", async (req, res) => {
     try {
-        const sales = await prisma.sale.findMany()
+        const sales = await prisma.sale.findMany({
+          include: {
+            product: true,
+          }
+        })
 
         res.status(200).json(sales)
     } catch (error) {
@@ -92,6 +96,61 @@ router.post("/", async (req, res) => {
       } catch (error) {
         return res.status(400).json({ error: error});
       }
+})
+
+router.patch("/:id", async (req, res) => {
+  const { id } = req.params
+  let priceCalculated = 0
+
+  try {
+    const valid = saleSchema.partial()
+    const validPartial = valid.safeParse(req.body)
+
+    if(!validPartial.success) {
+      res.status(400).json({error: validPartial.error})
+
+      return ;
+    }
+
+    if(validPartial.data.product) {
+      await prisma.productOnSales.deleteMany({
+        where: { saleId: Number(id) }
+      })
+      
+
+      for (let i = 0; i < validPartial.data.product.length; i++) {
+        const {id, quantity} = validPartial.data.product[i]
+        const product = await prisma.product.findUnique({where: {id: id}})
+
+        if(product) {
+          priceCalculated += Number(product.price) * quantity
+        }
+      }
+    }
+
+    const saleUpdate = await prisma.sale.update({
+      where: { id: Number(id) },
+      data: {
+        ...validPartial.data,
+        product: {
+          create: validPartial.data.product?.map((p: any) => {
+            return {
+              productId: p.id,
+              quantity: p.quantity
+            }
+          })
+        },
+        price: priceCalculated
+      },
+      include: {
+        product: true
+      }
+    })
+
+    res.status(201).json(saleUpdate)
+  } catch (error) {
+    res.status(400).json(error)
+  }
 })
 
 export default router
